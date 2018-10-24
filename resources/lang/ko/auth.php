8a3aa5dc3194ec5b07ee60a072ebd5b16b02a647
YeonJun Kwak <cezips@gmail.com>
2018-10-24 20:32:13 +0900

    한국어 설정 추가

---
+++ resources/lang/ko/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => '너무 많은 로그인 시도. :seconds 초 후에 다시 시도해 주세요.',
+
+];
