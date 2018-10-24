a426c4c974283ec4c9f52ef8063faba792e38d07
YeonJun Kwak <cezips@gmail.com>
2018-10-24 20:30:57 +0900

    광고 모델 추가

---
+++ app/Advertisement.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Advertisement extends Model
+{
+    /*protected $fillable = [
+        "title",
+        "code",
+        "file",
+        "position",
+        "sequence",
+        "width",
+        "height",
+        "link",
+        "link_title",
+        "start_date",
+        "end_date",
+    ];*/
+}
